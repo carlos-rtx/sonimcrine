5ff2fc61fdf1e07e7d1c08d3df3a25584c067e7c
Chärles <carlossebasev@gmail.com>
2026-05-23 15:57:24 -0600

    wdejknksw
    asdadaddasd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='body'>
+      <div>
+      <h1 className={`cormorant-garamond`}>Son im crine</h1>
+      <img src="..\realistic-sob-crying-emoji.gif" alt="Son im crine"/>
+      </div>
+    </div>
+  )
+}
+
+export default App
