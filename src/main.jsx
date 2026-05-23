ddfb516d1def19ac423bb5f777f1024a3c85ae60
Chärles <carlossebasev@gmail.com>
2026-05-23 16:11:07 -0600

    54

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-console.log("son")
+{console.log("son")}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
